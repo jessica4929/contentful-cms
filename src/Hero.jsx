91bb3e00afbcb14f4541fb2b3a168dc36db397aa
jessica4929 <jessica4929@gmail.com>
2024-05-07 12:34:15 -0500

    hero updates

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -5,14 +5,21 @@ const Hero = () => {
     <section className="hero">
       <div className="hero-center">
         <div className="hero-title">
-          <h1>Contentful CMS</h1>
+          <h1>React Projects</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis,
-            quo sunt temporibus earum mollitia dolor enim ab hic minima.
+            Check out a few of my projects from the Udemy's{" "}
+            <a
+              href="https://www.udemy.com/course/react-tutorial-and-projects-course/?referralCode=FEE6A921AF07E2563CEF&couponCode=ST20MT50724"
+              target="_blank"
+              rel="noreferrer"
+            >
+              React 18 Tutorial and Projects Course - 2024 Edition
+            </a>
+            .
           </p>
         </div>
         <div className="img-container">
-          <img src={heroImg} alt="woman and browser" />
+          <img src={heroImg} alt="woman and browser" className="img" />
         </div>
       </div>
     </section>
